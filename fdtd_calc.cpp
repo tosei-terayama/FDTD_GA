f42443a1050354a7542ef4527d38d17fb939976e
tosei-terayama <terayama@emlab.cei.uec.ac.jp>
2020-08-23 17:13:49 +0900

    test

--- fdtd_calc.cpp
+++ fdtd_calc.cpp
@@ -50,7 +50,6 @@ const double Azim{61.0*M_PI/180.0};
 void fdtd_calc(perturbation P_info, date ymd, geocoordinate lla_info,
             int Num_obs, geocoordinate* obs_p, double* Magnitude)
 {
-  std::cout << "OK" << std::endl;
   int time_step = 2000;
   double t;
   double J;
