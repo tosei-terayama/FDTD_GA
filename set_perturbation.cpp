e37eec6380cd8066cd783b00a962181e83fab3c7
tosei-terayama <terayama@emlab.cei.uec.ac.jp>
2020-08-23 22:03:41 +0900

    test

--- set_perturbation.cpp
+++ set_perturbation.cpp
@@ -14,17 +14,17 @@ void set_perturbation(perturbation P_info, double*** noise_Nh, double* Nh){
     double z_0{ dist(P_info.r0()) };
     double R_theta{ 0.0 };
     int lower_r = (int)Alt_lower_ionosphere/1.0e3;
-    double enhance{0.0};
 
     R_c(0) = z_0*std::sin(th(P_info.th0()))*std::cos(ph(P_info.phi0()));
     R_c(1) = z_0*std::sin(th(P_info.th0()))*std::sin(ph(P_info.phi0()));
     R_c(2) = z_0*std::cos(th(P_info.th0()));
 
+    std::cout << ion_L << std::endl;
+    
     for(int i = 0; i <= ion_L; i++){
         double z{ dist(i + lower_r) };
 
         for(int j = 0; j <= Ntheta; j++){
-            std::cout << j << " loop" << std::endl;
             for(int k = 0; k <= Nphi; k++){
                 R(0) = z*std::sin(th(j))*std::cos(ph(k));
                 R(1) = z*std::sin(th(j))*std::sin(ph(k));
@@ -40,7 +40,7 @@ void set_perturbation(perturbation P_info, double*** noise_Nh, double* Nh){
 
                 double d_h{ z_0*R_theta };
 
-                enhance = P_info.alpha()*std::exp(- (std::pow(d_h, 2.0)/2.0/std::pow(P_info.sig_h(), 2.0)))
+                double enhance = P_info.alpha()*std::exp(- (std::pow(d_h, 2.0)/2.0/std::pow(P_info.sig_h(), 2.0)))
                                             *std::exp(- (std::pow(z - z_0, 2.0)/2.0/std::pow(P_info.sig_r(), 2.0)));
 
                 noise_Nh[i][j][k] = Nh[i] + Nh[i] * enhance;
