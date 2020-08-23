4040719267f6d2346d8a5b87677589caa525ff71
tosei-terayama <terayama@emlab.cei.uec.ac.jp>
2020-08-23 22:07:33 +0900

    test

--- set_perturbation.cpp
+++ set_perturbation.cpp
@@ -19,9 +19,9 @@ void set_perturbation(perturbation P_info, double*** noise_Nh, double* Nh){
     R_c(1) = z_0*std::sin(th(P_info.th0()))*std::sin(ph(P_info.phi0()));
     R_c(2) = z_0*std::cos(th(P_info.th0()));
 
-    std::cout << ion_L << std::endl;
-    
-    for(int i = 0; i <= ion_L; i++){
+    int i{0};
+
+    for(i = 0; i <= ion_L; i++){
         double z{ dist(i + lower_r) };
 
         for(int j = 0; j <= Ntheta; j++){
@@ -46,6 +46,7 @@ void set_perturbation(perturbation P_info, double*** noise_Nh, double* Nh){
                 noise_Nh[i][j][k] = Nh[i] + Nh[i] * enhance;
             }
         }
+        std::cout << i << "loop" << std::endl;
     }
     std::cout << "loop OK" << std::endl;
 
