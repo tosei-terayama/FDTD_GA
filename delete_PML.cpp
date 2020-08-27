b22067af3f2a6015c992220885c3d83234334ef3
tosei-terayama <terayama@emlab.cei.uec.ac.jp>
2020-08-28 04:24:30 +0900

    delete test

--- delete_PML.cpp
+++ delete_PML.cpp
@@ -11,7 +11,7 @@ void delete_PML(
     double ****Hphi_r, double ****Hphi_theta)
 {
     std::cout << "PML delete start" << std::endl;
-    
+
     for(int i = 0; i <= 1; i++){
         delete_3d(Dr_theta1[i], Nr, L);
         delete_3d(Dr_theta2[i], Nr, L);
@@ -48,6 +48,7 @@ void delete_PML(
         delete_3d(Hphi_r[i], Nr, Ntheta - 2*L);
         delete_3d(Hphi_theta[i], Nr, Ntheta - 2*L);
     }
+    std::cout << "i : 2 ~ 3" << std::endl;
 
     delete[] Dr_theta1;
     delete[] Dr_theta2;
