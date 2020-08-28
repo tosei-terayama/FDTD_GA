c2aba679974c3965be01471d98a0b60e5db924cf
tosei-terayama <terayama@emlab.cei.uec.ac.jp>
2020-08-28 10:51:54 +0900

    GA test

--- calc_score.cpp
+++ calc_score.cpp
@@ -4,11 +4,11 @@
 #include <fstream>
 #include "GA_agent.h"
 
-double calc_score(double *Magnitude, double* Target_Magnitude, int size)
+double calc_score(double *Magnitude, double* Target_Magnitude, int Num)
 {
     double score{ 0.0 };
 
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i < Num; i++){
         score += std::abs(Magnitude[i] - Target_Magnitude[i]);
     }
 
--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ target_Pinfo.set_sigma(2.0e3, 60.0e3);
 /////////////////////////////////////////////
 */
 
-constexpr int Num_Individual { 8 };  // Number of individuals
+constexpr int Num_Individual { 4 };  // Number of individuals
 constexpr int Num_Generation { 30 };  // Number of generations to repeat
 constexpr int Num_Elete { 2 };  //  Number of elete
 constexpr double rnd_max { std::pow(2, 32) };  //   Max of mersenne twister (32 bit)
@@ -220,10 +220,8 @@ int main(int argc, char** argv){
                     = chromosome[CHILD][i*Nbit_total + j];
             }
         }
-        
-        if( rank == 0){
-            set_parameter(P_info, chromosome[CHILD]);
-        }
+
+        set_parameter(P_info, chromosome[CHILD]);
 
         if(flag == true) break;
 
