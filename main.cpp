e36a4a85e8f9b92c86f6f60c3585b57e98e11fdf
tosei-terayama <terayama@emlab.cei.uec.ac.jp>
2020-09-09 12:02:56 +0900

    GA test

--- main.cpp
+++ main.cpp
@@ -98,7 +98,7 @@ int main(int argc, char** argv){
     if(rank == 0){
         for(int i = 0; i < Num_Individual; i++){
             for(int j = 0; j < Nbit_total; j++){
-                std::cout << chromosome[i * Nbit_total + j] << " ";
+                std::cout << chromosome[0][i * Nbit_total + j] << " ";
             }
             std::cout << std::endl;
         }
