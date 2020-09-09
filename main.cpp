72cd9a33187087571377aec878c79a0bf585a2f4
tosei-terayama <terayama@emlab.cei.uec.ac.jp>
2020-09-09 13:16:17 +0900

    GA test

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ target_Pinfo.set_sigma(2.0e3, 60.0e3);
 /////////////////////////////////////////////
 */
 
-constexpr int Num_Individual { 24 };  // Number of individuals
+constexpr int Num_Individual { 32 };  // Number of individuals
 constexpr int Num_Generation { 40 };  // Number of generations to repeat
 constexpr int Num_Elete { 2 };  //  Number of elete
 constexpr double rnd_max { std::pow(2, 32) };  //   Max of mersenne twister (32 bit)
@@ -142,10 +142,6 @@ int main(int argc, char** argv){
     ifs.close();
 
     if( rank == 0 ) ofs_param << " # Ind   alpha  r  the  phi  sigma_r   sigma_h   score #" << std::endl;
-
-    if( rank == 0 ){
-        for(int i = 0; i < Num_obs; i++) std::cout << i << " " << Target_Magnitude[i] << std::endl;
-    }
 
     int child{ 0 };
     double judge{1.0e3};
