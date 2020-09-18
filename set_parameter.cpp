a05d31c61fa7bd9712ad78df495eea67ccc78d74
tosei-terayama <terayama@emlab.cei.uec.ac.jp>
2020-09-18 15:17:21 +0900

    09_18

--- set_parameter.cpp
+++ set_parameter.cpp
@@ -7,9 +7,10 @@ void set_parameter(perturbation* P_info, bool* chrom){
 
     bool bit1[Nbit_enhance];
     bool bit2[Nbit_alt];
-    bool bit3[Nbit_phi];
-    bool bit4[Nbit_sigr];
-    bool bit5[Nbit_sigh];
+    bool bit3[Nbit_th];
+    bool bit4[Nbit_phi];
+    bool bit5[Nbit_sigr];
+    bool bit6[Nbit_sigh];
 
     int count { 0 };
 
@@ -27,22 +28,27 @@ void set_parameter(perturbation* P_info, bool* chrom){
         P_info[i].set_alt( b2d(bit2, Nbit_alt, param2_min, param2_step) );
         count += Nbit_alt;
 
-        for(int j = 0; j < Nbit_phi; j++){
+        for(int j = 0; j < Nbit_th; j++){
             bit3[j] = chrom[i*Nbit_total + j + count];
         }
-        P_info[i].set_phi( b2d(bit3, Nbit_phi, param3_min, param3_step) );
+        P_info[i].set_th( b2d(bit3, Nbit_th, param3_min, param3_step) );
+
+        for(int j = 0; j < Nbit_phi; j++){
+            bit4[j] = chrom[i*Nbit_total + j + count];
+        }
+        P_info[i].set_phi( b2d(bit4, Nbit_phi, param4_min, param4_step) );
         count += Nbit_phi;
 
         for(int j = 0; j < Nbit_sigr; j++){
-            bit4[j] = chrom[i*Nbit_total + j + count];
+            bit5[j] = chrom[i*Nbit_total + j + count];
         }
-        P_info[i].set_sigr( b2d(bit4, Nbit_sigr, param4_min, param4_step) );
+        P_info[i].set_sigr( b2d(bit5, Nbit_sigr, param5_min, param5_step) );
         count += Nbit_sigr;
 
         for(int j = 0; j < Nbit_sigh; j++){
-            bit5[j] = chrom[i*Nbit_total + j + count];
+            bit6[j] = chrom[i*Nbit_total + j + count];
         }
-        P_info[i].set_sigh( b2d(bit5, Nbit_sigh, param5_min, param5_step) );
+        P_info[i].set_sigh( b2d(bit6, Nbit_sigh, param6_min, param6_step) );
         count = 0;
 
     }
